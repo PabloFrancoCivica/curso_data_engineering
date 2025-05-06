8240146c9078207344f32714fd881774b38ff69c
Pablo Franco <alumno26git@gmail.com>
2025-05-06 09:42:47 +0000

    Build-hecho de bases y stg_order_items

--- models/staging/sql_server_dbo/stg_sql_server_dbo__order_items.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__order_items.sql
@@ -7,9 +7,9 @@
 SELECT 
     order_item_id, --como hacer que esta es la PK
     order_id,
-    product_id,
+    oi.product_id,
     quantity,
     quantity * p.price AS total_price
-FROM {{ ref('sql_server_dbo', 'order_items') }} oi
-LEFT JOIN {{ ref('sql_server_dbo', 'products') }} p
+FROM {{ ref('base_sql_server_dbo__order_items') }} oi
+LEFT JOIN {{ ref('base_sql_server_dbo__products') }} p
     ON oi.product_id = p.product_id
